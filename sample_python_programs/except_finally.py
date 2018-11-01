d786165c0cd531bf5725e86ce46910ec5fc19ac8
gowtham81 <ic.gowtham@gmail.com>
2018-11-01 19:22:11 +0530

    Re-organize code

--- sample_python_programs/except_finally.py
+++
@@ -1,9 +0,0 @@
-try:
-    x = float(raw_input("Your number: "))
-    inverse = 1.0 / x
-except ValueError:
-    print ("You should have given either an int or a float")
-except ZeroDivisionError:
-    print ("Infinity")
-finally:
-    print("There may or may not have been an exception.")
--- sample_python_programs/fibonacci.py
+++
@@ -1,8 +0,0 @@
-def fib(n):
-    """
-    Calculates the n-th Fibonacci number iteratively
-    """
-    a, b = 0, 1
-    for i in range(n):
-        a, b = b, a + b
-    return a
